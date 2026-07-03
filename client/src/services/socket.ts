c0a8e327c050b539ed30ba9fdeea19ebaf9c52ce
ByRcHyZ <rafael.bieringer@gmail.com>
2026-07-03 20:48:38 +0200

    client changes, vite config

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,8 +1,5 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
-const serverUrl = import.meta.env.VITE_SERVER_URL || 'http://localhost:3001';
+const serverUrl = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
 
-export const socket = io(serverUrl, {
-  autoConnect: false,
-  transports: ['websocket', 'polling']
-});
+export const socket = io(serverUrl);
